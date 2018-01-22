ec0a16dcdcccd31be6e3df1cffe9d6fd3cf9569c
Karen Lo <karen_lo2@intuit.com>
2018-01-21 21:45:36 -0800

    add favicon and some sample input fields

--- src/components/Rsvp.vue
+++ src/components/Rsvp.vue
@@ -4,6 +4,20 @@
       <div class="title">
         RSVP
       </div>
+      <div class="deadline">
+        Please confirm by
+        <span class="date">
+          August 12, 2019!
+        </span>
+      </div>
+      <div class="form-fields">
+        <div>
+          Your Name <input v-model="name">
+        </div>
+        <div>
+          Guest Name <input v-model="name">
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -20,5 +34,16 @@ export default {
     background-color: pink;
     height: 100%;
     position: relative;
+  }
+  .deadline {
+    font-family: 'Amatic SC', cursive;
+    font-size: 30px;
+    .date {
+      font-weight: bold;
+      font-size: 35px;
+    }
+  }
+  .form-fields {
+    font-family: 'Josefin Slab', serif;
   }
 </style>
