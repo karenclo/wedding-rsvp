bf12c0fbceffa7d21ffaeeedfb3b3bd0f349ab58
Karen Lo <karen_lo2@intuit.com>
2018-03-11 22:41:46 -0700

    finish RSVP

--- src/components/Rsvp.vue
+++ src/components/Rsvp.vue
@@ -14,35 +14,40 @@
 
       <div class="form-fields" v-if="!showGuestInfo">
         <div>
-          Phone Number <input v-model="phoneNumber" placeholder="">
+          Phone Number: <input class="phone-number" v-model="phoneNumber" placeholder="xxxxxxxxxx">
           <br>
           {{error}}
         </div>
-        <button v-on:click="getGuestInfo">next</button>
+        <button v-on:click="getGuestInfo">NEXT</button>
       </div>
       <div class="guest-info" v-if="showGuestInfo">
-        Your Name: {{guestInfo.name}}
-        <br>
-        Your Guest: {{guestInfo.plusOne}}
-        <br>
-        Attending?
-        <toggle-button
-          @change="onChangeAttendance"
-          :value="guestInfo.isAttending" :color="{checked: '#33D4FF', unchecked: '#919192'}" :sync="true" :labels="{checked: 'Yes!', unchecked: 'No'}">
-        </toggle-button>
-        {{guestInfo.isAttending}}
-        <br>
-        <div v-show="guestInfo.isAttending">
+        <div class="guest-field">
+          Your Name:
+          <span class="guest-field-value">{{guestInfo.name}}</span>
+        </div>
+        <div class="guest-field">
+          Your Guest:
+          <input class="plus-one guest-field-value" v-model="guestInfo.plusOne">
+        </div>
+        <div class="guest-field">
+          Attendance Status:
+          <toggle-button class="toggle-btn"
+            @change="onChangeAttendance"
+            :value="guestInfo.isAttending" :color="{checked: '#3FE3A0', unchecked: '#919192'}" :sync="true"
+            :labels="{checked: 'Accept', unchecked: 'Decline'}" :width="77">
+          </toggle-button>
+        </div>
+        <div class="guest-field" v-show="guestInfo.isAttending">
           Staying at Hyatt Ziva Los Cabos?
-          <toggle-button
+          <toggle-button class="toggle-btn"
             @change="onChangeStay"
-            :value="false" :color="{checked: '#33D4FF', unchecked: '#919192'}" :sync="true" :labels="{checked: 'Yes!', unchecked: 'No'}">
+            :value="guestInfo.isStayingAtHyatt" :color="{checked: '#3FE3A0', unchecked: '#919192'}" :sync="true"
+                         :labels="{checked: 'Yes', unchecked: 'No'}" :width="56">
           </toggle-button>
         </div>
         <br>
 
-        <br>
-        <button v-on:click="saveGuestInfo">done</button>
+        <button v-on:click="saveGuestInfo">SAVE</button>
       </div>
     </div>
   </div>
@@ -53,22 +58,20 @@ import db from '../Database'
 
 let guestsRef = db.ref('guests')
 
-let data = {
-  guestInfo: {
-    name: '',
-    plusOne: '',
-    isAttending: '',
-    isStayingAtHyatt: ''
-  },
-  phoneNumber: '',
-  showGuestInfo: false,
-  error: ''
-}
-
 export default {
   name: 'Rsvp',
   data: () => {
-    return data
+    return {
+      guestInfo: {
+        name: '',
+        plusOne: '',
+        isAttending: false,
+        isStayingAtHyatt: false
+      },
+      phoneNumber: '',
+      showGuestInfo: false,
+      error: ''
+    }
   },
   firebase: () => {
     return {
@@ -76,23 +79,46 @@ export default {
     }
   },
   methods: {
-    getGuestInfo: function () {
-      // validate phone number
-      if (this.phoneNumber in this.guests[0]) {
-        let currGuest = this.guests[0][this.phoneNumber]
-        this.guestInfo.name = currGuest.name
-        this.guestInfo.plusOne = currGuest.plusOne
-        this.guestInfo.isAttending = currGuest.isAttending
-        this.guestInfo.isStayingAtHyatt = currGuest.isStayingAtHyatt
 
-        this.showGuestInfo = true
+    // Retrieve guest info based on phone number input
+    getGuestInfo: function () {
+      let that = this
+
+      if (!this.phoneNumber) {
+        that.error = 'Please enter a valid number'
       } else {
-        this.error = 'Oops! We couldn\'t find you!'
+        db.ref('/guests/' + this.phoneNumber).once('value').then(function (snapshot) {
+          // Snapshot did not contain any info
+          if (!snapshot.val()) {
+            that.error = 'Oops! We couldn\'t find you!'
+          } else {
+            that.error = ''
+            let currGuest = snapshot.val()
+
+            that.guestInfo.name = currGuest.name
+            that.guestInfo.plusOne = currGuest.plusOne
+            that.guestInfo.isAttending = currGuest.isAttending
+            that.guestInfo.isStayingAtHyatt = currGuest.isStayingAtHyatt
+
+            that.showGuestInfo = true
+          }
+        }, function () {
+          that.error = 'Oops! We ran into an error retrieving your info.'
+        })
       }
     },
 
     saveGuestInfo: function () {
+      this.error = ''
       this.showGuestInfo = false
+      let that = this
+
+      db.ref('/guests/' + this.phoneNumber).set({
+        name: that.guestInfo.name,
+        plusOne: that.guestInfo.plusOne,
+        isAttending: that.guestInfo.isAttending,
+        isStayingAtHyatt: that.guestInfo.isStayingAtHyatt
+      })
     },
 
     onChangeAttendance: function () {
@@ -123,5 +149,47 @@ export default {
   }
   .form-fields, .guest-info {
     font-family: 'Josefin Slab', serif;
+
+    .guest-field {
+      margin-top: 10px;
+      font-size: 18px;
+    }
+
+    .phone-number {
+      border-style: none;
+      height: 25px;
+      width: 110px;
+      text-align: center;
+      margin-left: 5px;
+    }
+
+    .guest-field-value, .phone-number {
+      font-family: 'Amatic SC', cursive;
+      font-size: 20px;
+      font-weight: bold;
+      color: #636376;
+    }
+
+    .plus-one {
+      border-style: none;
+      height: 25px;
+      width: 145px;
+      padding-left: 5px;
+    }
   }
+  button {
+    width: 120px;
+    height: 30px;
+    border-radius: 30px;
+    margin-top: 35px;
+    border-style: none;
+    font-size: 12px;
+    font-family: 'Josefin Slab', serif;
+
+  }
+
+  .toggle-btn {
+    font-size: 14px;
+  }
+
 </style>
